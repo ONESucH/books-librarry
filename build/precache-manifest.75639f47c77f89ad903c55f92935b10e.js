f2ae1399be8adc52869a6e345b2d79114caa43ba
Ilya <mac@MacBookPro.local>
2019-11-15 11:16:10 +0300

    add build files

---
+++ build/precache-manifest.75639f47c77f89ad903c55f92935b10e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5816eca7c5fddd460b5c3f461a2c2108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67457cced00096adf0f8",
+    "url": "/static/css/main.759eb6af.chunk.css"
+  },
+  {
+    "revision": "704e7c3e5d884f7adb72",
+    "url": "/static/js/2.dffedab7.chunk.js"
+  },
+  {
+    "revision": "67457cced00096adf0f8",
+    "url": "/static/js/main.eae48662.chunk.js"
+  },
+  {
+    "revision": "03f01d85529e34b56f3e",
+    "url": "/static/js/runtime-main.f02e343f.js"
+  }
+]);
